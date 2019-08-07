e5f4391eea36da8cf19da73e60289cea9c2ed700
Youn-Hee Han <yh21.han@gmail.com>
2019-08-07 23:30:58 +0900

    fdsfd

--- scripts/ubuntu/upbit_order_book_recorder_ubuntu.sh
+++ scripts/ubuntu/upbit_order_book_recorder_ubuntu.sh
@@ -3,3 +3,4 @@
 cd $HOME/git/trade
 
 $HOME/anaconda3/envs/trade/bin/python -m upbit.upbit_order_book_recorder >> $HOME/git/trade/logs/error/upbit_order_book_recorder.log 2>&1
+$HOME/anaconda3/envs/trade/bin/python -m predict.buy >> $HOME/git/trade/logs/error/buy.log 2>&1
