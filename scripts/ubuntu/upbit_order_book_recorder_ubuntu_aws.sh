a6da9a4b613d0d6adb7e317f7d0bfead866cb8a0
Youn-Hee Han <yh21.han@gmail.com>
2019-08-09 20:33:07 +0900

    ㅇㅁㅇㅁㄴㅇㅁㄴ

--- scripts/ubuntu/upbit_order_book_recorder_ubuntu_aws.sh
+++ scripts/ubuntu/upbit_order_book_recorder_ubuntu_aws.sh
@@ -3,5 +3,5 @@
 cd $HOME/git/trade
 
 $HOME/anaconda3/envs/trade/bin/python -m upbit.upbit_order_book_recorder >> $HOME/git/trade/logs/error/upbit_order_book_recorder.log 2>&1
-#sudo sync && echo 3 > /proc/sys/vm/drop_caches
 $HOME/anaconda3/envs/trade/bin/python -m predict.buy >> $HOME/git/trade/logs/error/buy.log 2>&1
+$HOME/anaconda3/envs/trade/bin/python -m db.statistics >> $HOME/git/trade/logs/error/statistics.log 2>&1
