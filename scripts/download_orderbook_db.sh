35e4d4346ca3c6501987095c4a03fff4377ab7a2
Youn-Hee Han <yh21.han@gmail.com>
2019-11-02 21:52:19 +0900

    ...

--- scripts/download_orderbook_db.sh
+++ scripts/download_orderbook_db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 cd $HOME/git/trade/db/
-ssh -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230 "cp /home/ubuntu/git/trade/db/upbit_order_book_info.db /home/ubuntu/git/trade/db/upbit_order_book_info.db.new"
+ssh -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230 "cp /home/ubuntu/git/trade/web/db/upbit_order_book_info.db /home/ubuntu/git/trade/web/db/upbit_order_book_info.db.new"
+ssh -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230 "cp /home/ubuntu/git/trade/web/db/upbit_buy_sell.db /home/ubuntu/git/trade/web/db/upbit_buy_sell.db.new"
 
 mv upbit_order_book_info.db upbit_order_book_info.db.old
-scp -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230:/home/ubuntu/git/trade/db/upbit_order_book_info.db.new ./upbit_order_book_info.db
+scp -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230:/home/ubuntu/git/trade/web/db/upbit_order_book_info.db.new ../web/db/upbit_order_book_info.db
+scp -i ~/yh21-han-aws-key-seoul.pem ubuntu@15.164.143.230:/home/ubuntu/git/trade/web/db/upbit_buy_sell.db.new ../web/db/upbit_buy_sell.db
