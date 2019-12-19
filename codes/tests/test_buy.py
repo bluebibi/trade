2e4e46b2cc0691e0a58281327abc59df52f92f43
Youn-Hee Han <yh21.han@gmail.com>
2019-12-19 09:22:47 +0900

    ...

--- codes/tests/test_buy.py
+++ codes/tests/test_buy.py
@@ -4,8 +4,6 @@ import unittest
 
 import pandas as pd
 
-from web.db.database import order_book_conn
-
 idx = os.getcwd().index("trade")
 PROJECT_HOME = os.getcwd()[:idx] + "trade"
 sys.path.append(PROJECT_HOME)
@@ -17,15 +15,20 @@ class TestBuy(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_x(self):
-        coin_name = "ADA"
-        upbit_data = UpbitOrderBookBasedData(coin_name)
-        df = pd.read_sql_table(upbit_data.order_book_class.__tablename__, order_book_conn)
-        df = df.drop(["id", "base_datetime", "collect_timestamp"], axis=1)
-        data = df.values
-        # x = upbit_data.get_original_dataset_for_buy()
-        print(data.shape)
-        print(data[0:10])
+    # def test_x(self):
+    #     coin_name = "ADA"
+    #     upbit_data = UpbitOrderBookBasedData(coin_name)
+    #     df = pd.read_sql_table(upbit_data.order_book_class.__tablename__, order_book_conn)
+    #     df = df.drop(["id", "base_datetime", "collect_timestamp"], axis=1)
+    #     data = df.values
+    #     # x = upbit_data.get_original_dataset_for_buy()
+    #     print(data.shape)
+    #     print(data[0:10])
+
+    def test_get_db_right_time_coin_names(self):
+        l = get_db_right_time_coin_names()
+        print(l)
+
 
 
 if __name__ == "__main__":
