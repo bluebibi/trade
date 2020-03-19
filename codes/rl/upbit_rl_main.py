70f75ca73fe8282d991ded57e9c5d4bf356bdcdd
Youn-Hee Han <yh21.han@gmail.com>
2020-03-19 19:19:18 +0900

    ...

--- codes/rl/upbit_rl_main.py
+++ codes/rl/upbit_rl_main.py
@@ -152,7 +152,7 @@ class UpbitEnvironment(gym.Env):
 
         if self.status is EnvironmentStatus.TRYING_BUY:
             if action is BuyerAction.BUY_HOLD:
-                reward = 0.0
+                reward = -1000.0
                 next_env_status = EnvironmentStatus.TRYING_BUY
 
             elif action is BuyerAction.MARKET_BUY:
@@ -172,7 +172,7 @@ class UpbitEnvironment(gym.Env):
                 self.hold_coin_unit_price = info_dic["coin_unit_price"]
                 self.hold_coin_krw = info_dic["coin_krw"]
                 self.hold_coin_quantity = info_dic["coin_quantity"]
-                reward = 0.0
+                reward = -1000.0
                 next_env_status = EnvironmentStatus.TRYING_SELL
 
             elif action is SellerAction.MARKET_SELL:
